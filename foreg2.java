34542237dbbffd6290476a2ea27929a79fac88e8
RakshaNaik212 <raksha.23cs111@sode-edu.in>
2025-04-04 10:14:14 +0530

    Add files via upload

---
+++ foreg2.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java2;
+
+/**
+ *
+ * @author SMV
+ */
+public class foreg2 {
+    public static void main(String[] args){
+        System.out.print("String: ");
+        char ch='a';
+        for(int i=0;i<3;i++){
+            System.out.print((char)(ch+i)+" ");
+        }
+        for(int i=1;i>=0;i--){
+            System.out.print((char)(ch+i)+" ");
+        }
+        System.out.println("\n");
+    }
+    
+}
+    
+    
+
---
+++ foreg3.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java2;
+
+/**
+ *
+ * @author SMV
+ */
+public class foreg3 {
+     public static void main(String[] args){
+        int num=1;
+       
+        for(int i=0;i<3;i++){
+            System.out.print((num+i)+" ");
+        }
+        for(int i=1;i>=0;i--){
+            System.out.print((num+i)+" ");
+        }
+        System.out.println("\n");
+    }
+    
+}
