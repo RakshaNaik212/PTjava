ba60c387b9db3e4d26f44b83141c1bf5186c10b7
RakshaNaik212 <raksha.23cs111@sode-edu.in>
2025-04-08 11:41:44 +0530

    Add files via upload

---
+++ array2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java3;
+
+/**
+ *
+ * @author SMV
+ */
+public class array2 {
+    public static void main(String[] args){
+        char[] vowels = {'a','e','i','o','u'};
+        //System.out.println(vowels.length);
+        for(int i=0;i<vowels.length;i++)
+        {
+            System.out.println(vowels[i]);
+        
+        }
+    }
+    
+}
